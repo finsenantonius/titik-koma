6858e3f8ff0c84e0f7c6264bdea6353d4fda7465
Finsen Antonius <v.finsen.antonius@adira.co.id>
2021-04-11 22:08:53 +0700

    user profile screen #2

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -10,95 +10,70 @@ import {
   SafeAreaView,
   StatusBar,
   Image,
+  StyleSheet,
 } from "react-native";
 import { Spacer } from "../components/Spacer";
-import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight && `padding-top: ${StatusBar.currentHeight}px`};
-  background-color: #fff;
+  background-color: whitesmoke;
 `;
 
 const Container = styled(View)`
   flex: 1;
-  background-color: #fff;
-  padding-top: 12px;
+  background-color: whitesmoke;
+`;
+
+const HeaderContainer = styled(View)`
+  height: 50px;
+  padding: 16px;
+  flex-direction: row;
+  background-color: whitesmoke;
 `;
 
 const ProfileContainer = styled(View)`
+  height: 150px;
+  border-radius: 20px;
+  padding: 12px;
   flex-direction: row;
-  padding-horizontal: 16px;
-  justify-content: space-between;
 `;
 
-const HeadingText = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  font-size: 14px;
-  color: #616161;
+const MenuContainer = styled(View)`
+  background-color: #fff;
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+  padding: 20px;
+  flex: 1;
+`;
+
+const ProfilePicture = styled(Image)`
+  width: 125px;
+  height: 125px;
+  border-radius: 15px;
+  margin-right: 12px;
 `;
 
 const Name = styled(Text)`
   font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  font-size: 18px;
-`;
-
-const ProfilePicture = styled(Image)`
-  width: 40px;
-  height: 40px;
-  border-radius: 10px;
-`;
-
-const SearchContainer = styled(View)`
-  flex-direction: row;
-  background-color: whitesmoke;
-  height: 60px;
-  border-radius: 10px;
-  margin-bottom: 20px;
-`;
-
-const Icon = styled(Feather)`
-  font-size: 22px;
-  align-self: center;
-  color: orange;
-  margin-horizontal: 10px;
-`;
-
-const SearchBar = styled(TextInput)`
-  flex: 1;
-  font-size: 16px;
-  font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  padding-right: 10px;
-`;
-
-const MainBanner = styled(View)`
-  height: 190px;
-  background-color: #0e4a86;
-  border-radius: 10px;
-  flex-direction: row;
-  margin-bottom: 20px;
-`;
-
-const BannerSection = styled(View)`
-  width: 50%;
-  padding: 20px;
-  margin-right: 20px;
-`;
-
-const BannerText = styled(Text)`
   font-size: 24px;
-  font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  color: #fff;
-  margin-bottom: 10px;
+  color: black;
+  margin-bottom: -5px;
 `;
 
-const Button = styled(TouchableOpacity)`
+const Bio = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.bodySemiBold};
+  color: #616161;
+`;
+
+const EditButton = styled(TouchableOpacity)`
   height: 40px;
-  width: 80%
+  width: 220px
   border-radius: 10px;
   background-color: orange;
   justify-content: center;
-  align-items: center
+  align-items: center;
 `;
 
 const ButtonText = styled(Text)`
@@ -107,152 +82,162 @@ const ButtonText = styled(Text)`
   color: #fff;
 `;
 
-const BannerImage = styled(Image)`
-  height: 190px;
-  width: 135px;
-`;
-
-const CourseHeader = styled(Text)`
-  font-size: 20px;
+const MenuTitleText = styled(Text)`
+  font-size: 22px;
   font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  color: #24253d;
-  margin-bottom: 10px;
+  color: #0e4a86;
 `;
 
-const CourseCard1 = styled(TouchableOpacity)`
-  height: 110px;
-  width: 180px;
-  background-color: #fff5e5;
-  border-radius: 10px;
+const MenuSection = styled(View)`
+  height: 180px;
+  border-radius: 10px
+  background-color: #FFF;
+  elevation: 2;
+  margin-bottom: 16px
+`;
+
+const Menu = styled(TouchableOpacity)`
   padding: 16px;
-`;
-
-const CourseCard2 = styled(TouchableOpacity)`
-  height: 110px;
-  width: 180px;
-  background-color: #fff1ef;
-  border-radius: 10px;
-`;
-
-const CourseCard3 = styled(TouchableOpacity)`
-  height: 110px;
-  width: 180px;
-  background-color: #eaf9fe;
-  border-radius: 10px;
-  padding: 16px;
-`;
-
-const CourseName = styled(Text)`
-  font-size: 18px;
-  font-family: ${(props) => props.theme.fonts.body};
-  color: #305f72;
-`;
-
-const CourseIcon = styled(Image)`
-  height: 50px;
-  width: 50px;
-  align-self: flex-end;
-`;
-
-const CompetitionBanner = styled(View)`
-  height: 120px;
-  background-color: #fbf5ff;
-  border-radius: 10px;
+  height: 60px;
   flex-direction: row;
-  elevation: 3;
-  padding-left: 20px;
+  justify-content: space-between;
 `;
 
-const CompetitionImage = styled(Image)`
-  height: 121px;
-  width: 80px;
-  margin-right: 20px;
+const Wrapper = styled(View)`
+  flex-direction: row;
 `;
 
-const CompetitionText = styled(Text)`
-  font-size: 20px;
-  font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  color: #305f72;
+const MenuText = styled(Text)`
+  font-size: 16px
+  font-family: ${(props) => props.theme.fonts.body};
+  color: black;
+  align-self: center
 `;
 
-const CompetitionText2 = styled(Text)`
-  font-size: 14px;
-  font-family: ${(props) => props.theme.fonts.bodySemiBold};
-  color: #616161;
-  margin-bottom: 4px;
-`;
-
-const CompetitionButton = styled(TouchableOpacity)`
-  height: 35px;
-  width: 60%
-  border-radius: 10px;
-  background-color: orange;
-  justify-content: center;
-  align-items: center
+const Border = styled(View)`
+  border-bottom-color: whitesmoke;
+  border-width: 0.3px;
 `;
 
 export const UserProfileScreen = ({ navigation }) => {
   return (
     <SafeArea>
-      <Container>
-        <ProfileContainer>
-          <View style={{ justifyContent: "center" }}>
-            <HeadingText>Welcome Back,</HeadingText>
-            <Name>Finsen Antonius</Name>
-          </View>
-
-          <ProfilePicture source={require("../../assets/giraffe.png")} />
-        </ProfileContainer>
-
-        <Spacer>
-          <SearchContainer>
-            <Icon name="search" />
-            <SearchBar placeholder="Search" />
-          </SearchContainer>
-
-          <MainBanner>
-            <BannerSection>
-              <BannerText>What do you want to learn today ?</BannerText>
-              <Button>
-                <ButtonText>Get Started</ButtonText>
-              </Button>
-            </BannerSection>
-            <BannerImage source={require("../../assets/character.png")} />
-          </MainBanner>
-
-          <CourseHeader>Course</CourseHeader>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              marginBottom: 20,
-            }}
-          >
-            <CourseCard1>
-              <CourseName>Javascript</CourseName>
-              <View>
-                <CourseIcon source={require("../../assets/js.png")} />
+      <ScrollView>
+        <Container>
+          <HeaderContainer>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <MaterialIcons name="arrow-back" style={styles.icon} />
+            </TouchableOpacity>
+          </HeaderContainer>
+          <ProfileContainer>
+            <ProfilePicture source={require("../../assets/giraffe.png")} />
+            <View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Name>Finsen Antonius</Name>
+                <MaterialIcons name="verified" style={styles.verified} />
               </View>
-            </CourseCard1>
-            <CourseCard3>
-              <CourseName>React</CourseName>
-              <CourseIcon source={require("../../assets/js.png")} />
-            </CourseCard3>
-          </View>
+              <Bio>Suka ngoding</Bio>
 
-          <CourseHeader>Challenges</CourseHeader>
-          <CompetitionBanner>
-            <CompetitionImage source={require("../../assets/character2.png")} />
-            <View style={{ padding: 16 }}>
-              <CompetitionText>10-days challenge</CompetitionText>
-              <CompetitionText2>Beginner Level</CompetitionText2>
-              <CompetitionButton>
-                <ButtonText>Join Now</ButtonText>
-              </CompetitionButton>
+              <EditButton>
+                <ButtonText>Edit Profile</ButtonText>
+              </EditButton>
             </View>
-          </CompetitionBanner>
-        </Spacer>
-      </Container>
+          </ProfileContainer>
+          <MenuContainer>
+            <MenuTitleText>Akun</MenuTitleText>
+            <MenuSection>
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="local-offer" style={styles.icon} />
+                  <MenuText>Kode Voucher</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+              <Border />
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="redeem" style={styles.icon} />
+                  <MenuText>Reward Saya</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+              <Border />
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="people-alt" style={styles.icon} />
+                  <MenuText>Undang Teman</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+            </MenuSection>
+
+            <MenuTitleText>Pusat Bantuan</MenuTitleText>
+            <MenuSection>
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="help-outline" style={styles.icon} />
+                  <MenuText>Bantuan</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+              <Border />
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="feedback" style={styles.icon} />
+                  <MenuText>Feedback</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+              <Border />
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="star-border" style={styles.icon} />
+                  <MenuText>Nilai Kami</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+            </MenuSection>
+
+            <MenuTitleText>Pengaturan</MenuTitleText>
+            <MenuSection style={{ height: 120 }}>
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="lock" style={styles.icon} />
+                  <MenuText>Ubah Password</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+              <Border />
+              <Menu>
+                <Wrapper>
+                  <MaterialIcons name="logout" style={styles.icon} />
+                  <MenuText>Keluar</MenuText>
+                </Wrapper>
+                <MaterialIcons name="chevron-right" style={styles.arrow} />
+              </Menu>
+            </MenuSection>
+          </MenuContainer>
+        </Container>
+      </ScrollView>
     </SafeArea>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    color: "black",
+    fontSize: 22,
+    marginRight: 12,
+    alignSelf: "center",
+  },
+  verified: {
+    color: "skyblue",
+    fontSize: 20,
+    marginLeft: 4,
+  },
+  arrow: {
+    color: "black",
+    fontSize: 22,
+    alignSelf: "center",
+  },
+});
